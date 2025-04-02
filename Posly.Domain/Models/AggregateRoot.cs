d5343c9ca02c6dbac2e1873bd247d6601ac9dd73
Pham Van Anh Dung <dungcodejob@gmail.com>
2025-04-03 01:00:55 +0700

    feat: add domain model

---
+++ Posly.Domain/Models/AggregateRoot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Posly.Domain.Models
+{
+    public class AggregateRoot<TId> : Entity<TId> where TId : notnull
+    {
+        public AggregateRoot(TId id) : base(id)
+        {
+        }
+    }
+
+}
